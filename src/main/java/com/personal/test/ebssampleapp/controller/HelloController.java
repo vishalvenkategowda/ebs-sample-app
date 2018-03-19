a9a84d565469fee492c0cc7efe236a10c8a33bb0
Vishal Venkategowda <venkategowdavishal@johndeere.com>
2018-03-19 14:59:02 -0500

    added props

--- src/main/java/com/personal/test/ebssampleapp/controller/HelloController.java
+++ src/main/java/com/personal/test/ebssampleapp/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.personal.test.ebssampleapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {	
 	
+	@Value("${env.prop}")
+	private String envProp;
+	
+	@Autowired
+	private ConfigurableEnvironment env;
+	
 	@RequestMapping("/welcome")
 	public String welcome() {
-		return "Welcome to my page";
+		return "Welcome to my page with env: "+envProp;
 	}
 	
 }
